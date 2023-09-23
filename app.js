3b732f3833a0ecb1959454a5883889ce62ac010f
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2023-09-23 19:26:44 +0530

    Add File Structure

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.get("/", (req,res) =>{
 	res.status(200).json({message:"Hello from Note Fusion API"});
 });
 
-app.listen(port, ()=>{
-	console.log(`Running on http://127.0.0.1:${port}`);
-});
+
+//DB Connection
+mongoose.connect(process.env.DB_URI, {
+	useNewUrlParser: true,
+  useUnifiedTopology: true,
+  family: 4,
+})
+.then(()=>{
+	app.listen(port,()=>{
+		console.log(`Running on http://127.0.0.1:${port}`);
+	});
+	console.log("Connected to DB");
+})
+.catch((err)=>{
+	console.log(err);
+})
